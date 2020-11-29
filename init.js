1452812def30cfce09e968c13d3c648603212163
Lvdianzhao <lv_workwith@163.com>
2020-11-29 23:49:06 +0800

    修改优化

--- init.js
+++ init.js
@@ -1,23 +1,25 @@
-let fs = require('fs')
-let path = require('path')
-let shell = require("shelljs")
+let fs = require("fs");
+// let path = require('path')
+let shell = require("shelljs");
 
 // 结果输出目录
-let outputDir = 'output'
+let outputDir = "output";
 
 // 所有npm包名
-let pack = require("./init_serve.json")
-shell.cd(outputDir)
-let errArr = []
+let pack = require("./init_serve.json");
+shell.cd(outputDir);
+let errArr = [];
 pack.forEach((item, index) => {
   shell.exec(`npm pack ${item}`, function (err) {
     if (err) {
-      errArr.push(item)
-      fs.writeFileSync("AA-log.txt", JSON.stringify(errArr))
-      if (index == pack.length - 1) {
-        console.log('下载完成')
-      }
+      errArr.push(item);
+      fs.writeFileSync("AA-log.txt", JSON.stringify(errArr));
     }
-  })
-})
-
+    if(errArr.length){
+      console.log(`有${errArr.length}个错误下载,请查看错误记录`);
+    }
+    if (index == pack.length - 1) {
+      console.log("==============下载完成======================");
+    }
+  });
+});
