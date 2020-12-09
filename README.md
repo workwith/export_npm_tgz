da4d2a13a6e955ec999c30cf0f13ca56b9e28126
Lvdianzhao <lv_workwith@163.com>
2020-12-09 20:30:28 +0800

    直接传如package.json格式

--- README.md
+++ README.md
@@ -1,20 +1,31 @@
 # output_tgz_npm
-下载tgz格式npm源，转到私有服务器上
 
-##### 打开cmd进入当前目录(需要安装nodejs环境)
+下载 tgz 格式 npm 源，转到私有服务器上
 
-​	运行 npm i；（下载项目依赖包）
+##### 打开 cmd 进入当前目录(需要安装 nodejs 环境)
 
-​	运行 node init.js  (执行init文件)
+直接把项目中 package.json 文件拷入 init_serve.json 文件，如果单独下载传入数组格式
+
+```
+[
+ "qs",
+ "axios",
+ "vuex",
+ "typescript"
+]
+```
+
+​ 运行 npm i；（下载项目依赖包）
+
+​ 运行 node init.js (执行 init 文件)
 
 等待打出“下载完成”即可
 
-​	日志和文件输出在output文件夹
+​ 日志和文件输出在 output 文件夹
 
-​		AA-log.txt为下载失败文件；
+​ AA-log.txt 为下载失败文件；
 
-​		需要手动使用   npm pack 包名   进行下载 （不加版本号）
+重新执行，如果再失败需要手动下载
+​ 需要手动使用 npm pack 包名 进行下载 （不加版本号）
 
-
-
-vue 和vue-template-compiler的版本必须一致（这里统一为 @2.6.12）
+vue 和 vue-template-compiler 的版本必须一致（这里统一为 @2.6.12）
--- init.js
+++ init.js
@@ -1,29 +1,92 @@
 let fs = require("fs");
 let shell = require("shelljs");
+let path = require('path');
+let type;
+
+try{
+  fs.statSync(path.join(__dirname, 'output'));
+  //如果可以执行到这里那么就表示存在了
+  type = true;
+}catch(e){
+  //捕获异常
+  type = false;
+}
 
 fs.mkdir("output", (error) => {
-  if (error) {
+  console.log(error);
+  if (error && !type) {
     console.log("创建文件失败");
     return false;
   }
   // 结果输出目录
-  let outputDir = "output";
+  let outputDir =path.resolve(__dirname,'output');
   // 所有npm包名
   let pack = require("./init_serve.json");
+  // let pack = require("./init_serve2.json");
   shell.cd(outputDir);
-  let errArr = [];
-  pack.forEach((item, index) => {
-    shell.exec(`npm pack ${item}`, function (err) {
-      if (err) {
-        errArr.push(item);
-        fs.writeFileSync("AA-log.txt", JSON.stringify(errArr));
-      }
-      if (errArr.length) {
-        console.log(`有${errArr.length}个错误下载,请查看错误记录`);
-      }
-      if (index == pack.length - 1) {
-        console.log("==============下载完成======================");
-      }
+  let errArr = []; //下载错误包
+  // 判断是否是数组
+  if (Array.isArray(pack)) {
+    pack.forEach((item, index) => {
+      shell.exec(`npm pack ${item}`, function (err) {
+        if (err) {
+          errArr.push(item);
+          fs.writeFileSync("AA-log.txt", JSON.stringify(errArr));
+        }
+        // if (errArr.length) {
+        //   console.log(`有${errArr.length}个错误下载,请查看错误记录`);
+        // }
+        // if (index == pack.length - 1) {
+        //   console.log("==============下载完成======================");
+        // }
+      });
     });
-  });
+  } else {
+    let dependencies = pack.dependencies;
+    let devDependencies = pack.devDependencies;
+    if (dependencies) {
+      for (let key in dependencies) {
+        // console.log(key + "---" + pack[key]);
+        // console.log(pack[key].indexOf("^"));
+        if (
+          dependencies[key].indexOf("^") != -1 ||
+          dependencies[key].indexOf("~") != -1
+        ) {
+          let newstr = dependencies[key].slice(1);
+          updown(key, newstr, errArr);
+        } else {
+          let newstr = dependencies[key];
+          updown(key, newstr, errArr);
+        }
+      }
+    }
+    if (devDependencies) {
+      for (let key in devDependencies) {
+        // console.log(key + "---" + pack[key]);
+        // console.log(pack[key].indexOf("^"));
+        if (
+          devDependencies[key].indexOf("^") != -1 ||
+          devDependencies[key].indexOf("~") != -1
+        ) {
+          let newstr = devDependencies[key].slice(1);
+          updown(key, newstr, errArr);
+        } else {
+          let newstr = devDependencies[key];
+          updown(key, newstr, errArr);
+        }
+      }
+    }
+  }
+  if (errArr.length) {
+    console.log(`有${errArr.length}个错误下载,请查看错误记录`);
+  }
 });
+
+function updown(key, newstr, errArr) {
+  shell.exec(`npm pack ${key}@${newstr}`, function (err) {
+    if (err) {
+      errArr.push(key);
+      fs.writeFileSync("AA-log.txt", JSON.stringify(errArr));
+    }
+  });
+}
